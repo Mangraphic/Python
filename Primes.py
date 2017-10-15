7d3c5f303337f5176cfc9d991560b21a1eaac878
Mangraphic <mangraphicz@gmail.com>
2017-10-15 14:52:29 +0200

    fixed code
    apparently the same code as before wasn't working anymore, so I had to change it a bit

--- Primes.py
+++ Primes.py
@@ -3,9 +3,11 @@ v = int(input("Show non-prime numbers? (1 = YES    0 = NO) "))
 
 for n in range(2,t+1):
 	for x in range(2,n):
-		if v == 1:
-			if n % x == 0:
+		if n % x == 0:
+			if v == 1:
 				print(n, 'equals', x, '*', n//x)
+				break
+			elif v == 0:
 				break
 	else:
 		print(n,"is a prime number")
