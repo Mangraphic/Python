1228b26d664e7fa54d98a4a2829e60a187680b16
Mangraphic <32812873+Mangraphic@users.noreply.github.com>
2017-10-15 14:53:08 +0200

    Update Primes.py
    Described

--- Primes.py
+++ Primes.py
@@ -1,5 +1,5 @@
 t = int(input("Limit: "))
-v = int(input("Show non-prime numbers? (1 = YES    0 = NO) "))
+v = int(input("Show non-prime numbers? (1 = YES    0 = NO) ")) #reason why is shown in console
 
 for n in range(2,t+1):
 	for x in range(2,n):
